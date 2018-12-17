470cd39ca5646d73dd322ac4e2f3a636e5c0d6b7
Jeremiah van Oosten <jpvanoosten@hotmail.com>
2018-12-17 16:15:56 +0100

    Export GameLib as DLL (by default).

--- GameLib/inc/Game.h
+++ GameLib/inc/Game.h
@@ -1,4 +1,15 @@
-class Game
+#if defined(GAME_EXPORTS)
+#   define GAME_DLL __declspec(dllexport)
+#   define GAME_EXTERN
+#elif defined(GAME_IMPORTS)
+#   define GAME_DLL __declspec(dllimport)
+#   define GAME_EXTERN extern
+#else
+#   define GAME_DLL
+#   define GAME_EXTERN
+#endif
+
+class GAME_DLL Game
 {
 public:  
     Game();
